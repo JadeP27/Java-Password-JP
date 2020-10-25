abde4c0a77d8a3de388b5871244ef2f02d0db331
JadeP <jnporter.jp@gmail.com>
2020-10-25 02:30:06 -0400

    saving final changes

--- Develop/script.js
+++ Develop/script.js
@@ -7,8 +7,68 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword () {
+  
+  // confirm("Please select password criteria");
+  var pwConfirm = confirm("Please confirm password criteria");
+  if(pwConfirm === true){
+    alert("Use ok or cancel to update your password characteristics...");
+  } else alert("Undefined");
+
+  //variables
+  // var finalPassword = ;
+  options = [];
+  upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  numChars = Math.floor(Math.random() * 10);
+  specChar = ["/", ".", ";", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", ":", "<", ">", "?", "-", "_","`", "~", "+", "{", "}"];
+  
+  
+  // prompts
+  var upInput = confirm("would you like your password to contain a an uppercase character?");
+  var lowInput = confirm("Would you like your password to contain a lowercase character?");
+  var numInput = confirm("Would you like your password to contain a number?");
+  var specInput = confirm("Would you like your password to contain a special character?");
+  
+  // length
+  var lengthOfPassword = parseInt(prompt("Please select your password length"));
+  
+  // upperCharacters
+    if(upInput === true){
+      function random () {
+        index = Math.floor(Math.random()*upperChars[0].length);
+        options = (upperChars[0][index]);
+    } } 
+   
+  // lowerCharacters
+    if(lowInput === true){
+      function random () {
+        index = Math.floor(Math.random()*lowerChars[0].length);
+        options = (lowerChars[0][index]);
+   } }
+  
+  // numbers
+    if(numInput === true){
+      function random () {
+        options = Math.floor(Math.random() * 10);  
+   } }
+  
+  // specials chaacters
+  if(specInput === true){
+    function random () {
+      index = Math.floor(Math.random()*specChars[0].length);
+      options = (specChars[0][index]);
+   } }
+  
+  //length for loop
+  for(var i = 8; i < lengthOfPassword; i++){
+    finalPassword = options
+  }
+
+  return finalPassword;
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Java-Password-JP
+
+## Table of Contents:
+
+   * Description
+
+   * Installation
+
+   * Usage
+
+### Description
+
+* Java-Password-JP is a client requested project     intended to generate a random password for the user based on the user input from previously installed prompts.  
+  
+
+#### Installation
+
+* No installations are required in order to access the prompts or any links utilized for this project.  As long as you have access to the internet you should be able to access the page and the final password.
+
+##### Usage
+
+* Unfortunately the code doesn't return the final password.
+
